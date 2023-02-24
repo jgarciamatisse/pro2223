f8ca6185a2e14cf9d373652c610e03b9c8f343d1
jgarcia <jgarcia@alumno-1dam>
2023-02-24 08:42:55 +0100

    examen año pasdo

--- proJavi.04Listas/src/_03ejercicios/_06ActualizarCorreos.java
+++ proJavi.04Listas/src/_03ejercicios/_06ActualizarCorreos.java
@@ -2,20 +2,42 @@ package _03ejercicios;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class _06ActualizarCorreos {
 	public static void main(String[] args) {
-		ArrayList<String> actual = new ArrayList<>(Arrays.asList("b","c","d","f","g"));
+		ArrayList<String> actual = new ArrayList<>(Arrays.asList("b","c","d","f","g","h"));
 		ArrayList<String> correos = new ArrayList<>(Arrays.asList(
 				"a@ieshenrimatisse.es",
 				"b@ieshenrimatisse.es",
 				"c@ieshenrimatisse.es",
 				"d@ieshenrimatisse.es",
-				"f@ieshenrimatisse.es",
-				"g@ieshenrimatisse.es"
+				"e@ieshenrimatisse.es",
+				"f@ieshenrimatisse.es"
 		));
 	
-		//Actualizar la lista correos
+		//Se pide actualizar la lista correos
+		
+		//Eliminar correos cuyo usuario no este en actual
+		Iterator<String> it  = correos.iterator();
+		while (it.hasNext()) {
+			String cuenta = it.next();
+			String usuario = cuenta.substring(0,cuenta.indexOf("@"));
+			if(!actual.contains(usuario)) {
+				it.remove();
+			}
+		}
+		
+		//Añadir los correos de los usuarios de actual que no estén en correos
+		for(String usuario: actual) {
+			String email = usuario +"@ieshenrimatisse.es";
+			if(!correos.contains(email)) {
+				correos.add(email);
+			}
+		}
+		
+		//Mostramos la lista actualizada
+		System.out.println(correos);
 		
 		
 	}
