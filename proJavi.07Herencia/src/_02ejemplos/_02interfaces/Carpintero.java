135ecb7c5d4b8a915f342c8b020050d36700f68c
jgarcia <jgarcia@alumno-1dam>
2023-03-13 11:18:11 +0100

    clases abstractas

--- proJavi.07Herencia/src/_02ejemplos/_02interfaces/Carpintero.java
+++ proJavi.07Herencia/src/_02ejemplos/_02interfaces/Carpintero.java
@@ -1,5 +1,13 @@
 package _02ejemplos._02interfaces;
 
-public class Carpintero {
-
+//Cuando una clase implements un interface, está obligada
+//a implementar los métodos definidos en el interface
+//(o bien, a ser abstracta si no los implementa)
+public class Carpintero implements Montador {
+	public void montar(String s) {
+		//Codigo del metodo
+	}
+	public void desmontar(String s) {
+		//Codigo del metodo
+	}
 }
--- proJavi.07Herencia/src/_02ejemplos/_02interfaces/Montador.java
+++ proJavi.07Herencia/src/_02ejemplos/_02interfaces/Montador.java
@@ -1,5 +1,7 @@
 package _02ejemplos._02interfaces;
 
-public class Montador {
-
+public interface Montador {
+	public abstract void montar(String s);
+	//public y abstract se puede (suele) omitir
+	/*public abstract*/ void desmontar(String s);
 }
